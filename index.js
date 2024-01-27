8bf01c125ea88a2c98d03c39709194a94a4b322b
xiron <xiron7077@gmail.com>
2024-01-27 16:11:05 +0500

    Changed nav design and improved buttton

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("menu-button").addEventListener(
         "click",
         () => {
-            document.getElementById("menu").hidden = true;
-            document.getElementById("hidden-menu").hidden = false;
-        },
-        false
-    );
-
-    document.getElementById("hidden-menu-button").addEventListener(
-        "click",
-        () => {
-            document.getElementById("menu").hidden = false;
-            document.getElementById("hidden-menu").hidden = true;
+            buttonText = document.getElementById("menu-button").textContent;
+            if(buttonText === "☰") {
+                document.getElementById("hidden-menu").hidden = false;
+                document.getElementById("menu-button").textContent = "⚟";
+            } else {
+                document.getElementById("hidden-menu").hidden = true;
+                document.getElementById("menu-button").textContent = "☰";
+            }
         },
         false
     );
